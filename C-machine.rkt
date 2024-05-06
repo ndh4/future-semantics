a0fe053e613519eaec9699232148e23003e4f6e1
ndh4 <ndh4@rice.edu>
2024-05-06 15:49:12 -0500

    Write out initial questions about C-machine implementation

---
+++ C-machine.rkt
@@ -0,0 +1,42 @@
+#lang racket
+
+(require redex)
+
+;; ??? Do we need to define keywords (let, future, car, cdr, if, apply, cons) somewhere else?
+
+(define-language Λa
+  (S ::= M error)
+  (M ::= V
+        (let (x V) M)
+        (let (x (future M)) M)
+        (let (x (car V)) M)
+        (let (x (cdr V)) M)
+        (let (x (if V M M)) M)
+        (let (x (apply V V)) M)
+        (let (x M) M))
+  (V ::= c x (λ (x) M) (cons x y))
+  (F ::= V error)
+  (A ::= c procedure (cons A A))
+  (c ::= nil natural)
+  (E ::= hole
+         (let (x E) M)
+         (let (x (future E)) M)
+;; ??? Should error be a type of evaluation context?
+         )
+  (x y z ::= variable-not-otherwise-mentioned)
+  #:binding-forms
+  (λ (x) M #:refers-to x)
+  
+;; ??? How do we deal with non-lambda binding forms? (let)
+  
+  ;#:binding-forms
+  ;(let (x V) M #:refers-to x)
+  )
+
+(define -->c
+  (reduction-relation
+   Λa
+   [--> (in-hole E (let (x V) M))
+        (in-hole E (substitute M x V))
+        bind]))
+
