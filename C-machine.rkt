e24842900e39183e56ba0ed0be0acd49434bc2ec
ndh4 <ndh4@rice.edu>
2024-05-13 15:00:52 -0500

    Build out transition rules for C-machine

--- C-machine.rkt
+++ C-machine.rkt
@@ -2,11 +2,9 @@
 
 (require redex)
 
-;; ??? Do we need to define keywords (let, future, car, cdr, if, apply, cons) somewhere else? A: No
-
 (define-language Λa
   (S ::= M error)
-  (M ::= V
+  (M N ::= V
         (let (x V) M) ; Might need to remove this line
         (let (x (future M)) M)
         (let (x (car V)) M)
@@ -20,19 +18,17 @@
   (c ::= nil natural)
   (E ::= hole
          (let (x E) M)
-         (let (x (future E)) M)
-;; ??? Should error be a type of evaluation context?
-         )
+         (let (x (future E)) M))
   (x y z ::= variable-not-otherwise-mentioned)
   #:binding-forms
   (λ (x) M #:refers-to x)
   (let (x V) M #:refers-to x)
+  (let (x (future M_1)) M_2 #:refers-to x)
+  (let (x (car V)) M #:refers-to x)
+  (let (x (cdr V)) M #:refers-to x)
+  (let (x (if V M_1 M_2)) M_3 #:refers-to x)
+  (let (x (apply V_1 V_2)) M #:refers-to x)
   (let (x M_1) M_2 #:refers-to x)
-  
-;; ??? How do we deal with non-lambda binding forms? (let)
-  
-  ;#:binding-forms
-  ;(let (x V) M #:refers-to x)
   )
 
 (redex-match Λa (let (x M_1) M_2) (term (let (x 3) x)))
@@ -43,5 +39,44 @@
    Λa
    [--> (in-hole E (let (x V) M))
         (in-hole E (substitute M x V))
-        bind]))
+        bind]
+   [--> (in-hole E (let (x (future V)) M))
+        (in-hole E (substitute M x V))
+        future-id]
+   
+   [--> (in-hole E (let (x (car (cons V_1 V_2))) M))
+        (in-hole E (substitute M x V_1))
+        car]
+   [--> (in-hole E (let (x (car V)) M))
+        error
+        (side-condition
+         (not (redex-match? Λa (cons V_1 V_2) (term V))))
+        car-error]
+   
+   [--> (in-hole E (let (x (cdr (cons V_1 V_2))) M))
+        (in-hole E (substitute M x V_2))
+        cdr]
+   [--> (in-hole E (let (x (cdr V)) M))
+        error
+        (side-condition
+         (not (redex-match? Λa (cons V_1 V_2) (term V))))
+        cdr-error]
+
+   [--> (in-hole E (let (x (if V M_1 M_2)) M))
+        (in-hole E (let (x M_1) M))
+        (side-condition
+         (not (redex-match? Λa nil (term V))))
+        if-first]
+   [--> (in-hole E (let (x (if nil M_1 M_2)) M))
+        (in-hole E (let (x M_2) M))
+        if-second]
+   
+   [--> (in-hole E (let (x (apply (λ (y) N) V_2)) M))
+        (in-hole E (let (x (substitute N y V_2)) M))
+        apply]
+   [--> (in-hole E (let (x (apply V_1 V_2)) M))
+        error
+        (side-condition
+         (not (redex-match? Λa (λ (y) N) (term V_1))))
+        apply-error]))
 
